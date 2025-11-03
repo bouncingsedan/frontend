a609fd50a942b97955054ee7db063bbe8c1d42f0
bouncingsedan <240913420+bouncingsedan@users.noreply.github.com>
2025-11-03 14:10:39 +1100

    refactor(contacts): reflect backend /contact GET response change

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -25,7 +25,7 @@ export default async function Page() {
     );
   }
 
-  const contacts: Contact[] = (await getAllContacts.json()).rows;
+  const contacts: Contact[] = (await getAllContacts.json());
 
   const hasContacts = contacts.length > 0;
 
